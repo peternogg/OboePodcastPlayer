0eb9adb5f1ebc454736fca63da091a23e5bd5607
Peter Higginbotham <peternogg@gmail.com>
2019-01-16 14:53:33 -0800

    UI Sketch in place, with navigation interactions
    General outline of UI layout is in place. Most things live in the
    central stack, and the side buttons move between pages. They'll
    need icons, rather than the text they have now.

--- OboeWindow.cpp
+++ OboeWindow.cpp
@@ -6,6 +6,28 @@ OboeWindow::OboeWindow(QWidget *parent) :
     ui(new Ui::OboeWindow)
 {
     ui->setupUi(this);
+
+    // Set up naviation actions
+    ui->navSubscriptions->setDefaultAction(ui->goToSubscriptions);
+    ui->navSearch->setDefaultAction(ui->goToSearch);
+    ui->navPlaylists->setDefaultAction(ui->goToPlaylists);
+    ui->navNowPlaying->setDefaultAction(ui->goToNowPlaying);
+
+    connect(ui->goToSubscriptions, &QAction::triggered, [this]() {
+       this->ui->stackedWidget->setCurrentIndex(0);
+    });
+
+    connect(ui->goToSearch, &QAction::triggered, [this]() {
+       this->ui->stackedWidget->setCurrentIndex(1);
+    });
+
+    connect(ui->goToPlaylists, &QAction::triggered, [this]() {
+       this->ui->stackedWidget->setCurrentIndex(3);
+    });
+
+    connect(ui->goToNowPlaying, &QAction::triggered, [this]() {
+       this->ui->stackedWidget->setCurrentIndex(4);
+    });
 }
 
 OboeWindow::~OboeWindow()
