1d0974d44d416faf212785ebe25e68ed76b26972
Peter Higginbotham <peternogg@gmail.com>
2019-02-15 14:44:53 -0800

    Added download/listening fields to PodcastItem

--- player/include/PodcastItem.h
+++ player/include/PodcastItem.h
@@ -14,13 +14,22 @@ public:
     PodcastItem(feedpp::item const& item, QObject *parent = nullptr);
     virtual ~PodcastItem();
 
+    enum class ListenedState { Unheard, InProgress, Finished };
+    enum class DownloadState { NotDownloaded, Downloading, Downloaded };
+
+    Q_ENUM(ListenedState)
+    Q_ENUM(DownloadState)
+
     Q_PROPERTY(QString title READ title)
     Q_PROPERTY(QString description READ description)
     Q_PROPERTY(QDateTime pubDate READ pubDate)
     Q_PROPERTY(QString guid READ guid)
     Q_PROPERTY(QUrl enclosureUrl READ enclosureUrl)
     Q_PROPERTY(QString enclosureType READ enclosureType)
-
+    Q_PROPERTY(QString downloadPath READ downloadPath WRITE setDownloadPath)
+    Q_PROPERTY(qint64 lastTimestamp READ lastTimestamp WRITE setLastTimestamp)
+    Q_PROPERTY(ListenedState listenedState READ listenedState WRITE setListenedState)
+    Q_PROPERTY(DownloadState downloadState READ downloadState WRITE setDownloadState)
 
     QString title() const;
     QString description() const;
@@ -28,6 +37,21 @@ public:
     QString guid() const;
     QUrl enclosureUrl() const;
     QString enclosureType() const;
+    QString downloadPath() const;
+    qint64 lastTimestamp() const;
+    ListenedState listenedState() const;
+    DownloadState downloadState() const;
+
+    void setTitle(const QString& title);
+    void setDescription(const QString& description);
+    void setPubDate(const QDateTime& pubDate);
+    void setGuid(const QString& guid);
+    void setEnclosureUrl(const QUrl& enclosureUrl);
+    void setEnclosureType(const QString& enclosureType);
+    void setDownloadPath(const QString& downloadPath);
+    void setLastTimestamp(const qint64& lastTimestamp);
+    void setListenedState(const ListenedState& listenedState);
+    void setDownloadState(const DownloadState& downloadState);
 
 private:
     QString _title;
@@ -36,7 +60,10 @@ private:
     QString _guid;
     QUrl _enclosureUrl;
     QString _enclosureType;
-
+    QString _downloadPath;
+    qint64 _lastTimestamp;
+    ListenedState _listenedState;
+    DownloadState _downloadState;
 };
 
 #endif // PODCASTITEM_H
--- player/source/PodcastItem.cpp
+++ player/source/PodcastItem.cpp
@@ -7,7 +7,10 @@ PodcastItem::PodcastItem(feedpp::item const& data, QObject *parent)
       _pubDate{QDateTime::fromString(QString::fromStdString(data.pubDate))},
       _guid{QString::fromStdString(data.guid)},
       _enclosureUrl{QString::fromStdString(data.enclosure_url)},
-      _enclosureType{QString::fromStdString(data.enclosure_type)}
+      _enclosureType{QString::fromStdString(data.enclosure_type)},
+      _downloadPath{},
+      _listenedState{ListenedState::Unheard},
+      _downloadState{DownloadState::NotDownloaded}
 {
 
 }
@@ -42,4 +45,74 @@ QUrl PodcastItem::enclosureUrl() const
 QString PodcastItem::enclosureType() const
 {
     return _enclosureType;
+}
+
+void PodcastItem::setTitle(const QString& title)
+{
+    _title = title;
+}
+
+void PodcastItem::setDescription(const QString& description)
+{
+    _description = description;
+}
+
+void PodcastItem::setPubDate(const QDateTime& pubDate)
+{
+    _pubDate = pubDate;
+}
+
+void PodcastItem::setGuid(const QString& guid)
+{
+    _guid = guid;
+}
+
+void PodcastItem::setEnclosureUrl(const QUrl& enclosureUrl)
+{
+    _enclosureUrl = enclosureUrl;
+}
+
+void PodcastItem::setEnclosureType(const QString& enclosureType)
+{
+    _enclosureType = enclosureType;
+}
+
+QString PodcastItem::downloadPath() const
+{
+    return _downloadPath;
+}
+
+void PodcastItem::setDownloadPath(const QString& downloadPath)
+{
+    _downloadPath = downloadPath;
+}
+
+qint64 PodcastItem::lastTimestamp() const
+{
+    return _lastTimestamp;
+}
+
+void PodcastItem::setLastTimestamp(const qint64& lastTimestamp)
+{
+    _lastTimestamp = lastTimestamp;
+}
+
+PodcastItem::ListenedState PodcastItem::listenedState() const
+{
+    return _listenedState;
+}
+
+void PodcastItem::setListenedState(const ListenedState& listenedState)
+{
+    _listenedState = listenedState;
+}
+
+PodcastItem::DownloadState PodcastItem::downloadState() const
+{
+    return _downloadState;
+}
+
+void PodcastItem::setDownloadState(const DownloadState& downloadState)
+{
+    _downloadState = downloadState;
 }
